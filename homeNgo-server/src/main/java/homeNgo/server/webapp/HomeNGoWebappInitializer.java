eda0cc1d6131b5c57fbba610e86a4525001b1b8f
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-11-06 13:45:28 -0500

    Add Logger

--- homeNgo-server/src/main/java/homeNgo/server/webapp/HomeNGoWebappInitializer.java
+++ homeNgo-server/src/main/java/homeNgo/server/webapp/HomeNGoWebappInitializer.java
@@ -10,18 +10,24 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HomeNGoWebappInitializer implements WebApplicationInitializer {
+	private static Logger LOGGER = Logger
+			.getLogger(HomeNGoWebappInitializer.class.getCanonicalName());
+
 	public void onStartup(ServletContext servletContext)
 			throws ServletException {
 		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
 		ctx.register(WebappConfig.class);
 		ctx.setServletContext(servletContext);
 
-		ServletRegistration.Dynamic dispatcher =
-				servletContext.addServlet("dispatcher",
-						new DispatcherServlet(ctx));
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
+				"dispatcher", new DispatcherServlet(ctx));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/");
+		LOGGER.log(Level.INFO, "HomeNGo WebApp Started");
 	}
 
 }
